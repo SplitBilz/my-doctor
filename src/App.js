aade3dc34bf07c48e90f54101902192037551a44
hnobi <hnobi08@yahoo.com>
2024-05-18 18:41:30 +0100

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Login from './pages/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1> Landing page </h1>
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+ 
+])
+
 
 function App() {
   return (
-    <div className="App">
-            hello
-    </div>
+     <RouterProvider  router={router}/>
   );
 }
 
